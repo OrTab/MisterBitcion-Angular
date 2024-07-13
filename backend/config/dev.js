c393cd73e11447ef2bb859449e599a5f2eca387a
OrTab <74709655+OrTab@users.noreply.github.com>
2024-07-14 02:11:48 +0300

    Update dev.js

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,4 +1,3 @@
 module.exports = {
-  'dbURL': 'mongodb://localhost:27017',
-  // 'dbURL': 'mongodb+srv://ortab:D2qS1GNyEOSnMmxp@cluster0.ztjvs.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  'dbURL': '',
 }
